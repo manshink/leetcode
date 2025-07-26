cbe4d89d025c4b53e975d2997a09841763a90abc
Mateo Roldan <mroldantaborda1@gmail.com>
2025-07-25 23:21:30 -0500

    Time: 46 ms (30.14%), Space: 53.2 MB (68.74%) - LeetHub

--- 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -4,13 +4,15 @@
  */
 var expect = function(val) {
     return {
-        toBe : (val2) => {
-            if ( val !== val2 ) throw new Error ("Not Equal"); 
-            else return true;
+        toBe: (val2) => {
+            if(val !== val2)
+            throw new Error("Not Equal");
+            return true;
         },
         notToBe: (val2) => {
-            if (val === val2) throw new Error("Equal"); 
-            else return true;
+            if(val === val2)
+            throw new Error("Equal");
+            return true;
         }
     }
 };
